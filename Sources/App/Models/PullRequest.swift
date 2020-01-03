daeeac02eb1273f3c7009a93a9ce546b9285dec6
redamimouni <rmimouni@octo.com>
2020-01-03 17:52:09 +0100

    Handle different event types

--- Sources/App/Models/PullRequest.swift
+++ Sources/App/Models/PullRequest.swift
@@ -1,7 +1,20 @@
-struct PullRequest: Decodable {
+import Vapor
+
+struct Payload: Content {
     let review: Review
+    let pull_request: PullRequest
 }
 
-struct Review: Decodable {
+struct Review: Content {
     let pull_request_url : String
+}
+
+struct PullRequest: Content {
+    let number: Int
+}
+
+enum EventType: String {
+    case STATUS = "status"
+    case PULL_REQUEST = "pull_request_review"
+    case UNKNOWN = "unknown"
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,8 +16,21 @@ public func routes(_ router: Router) throws {
     let todoController = TodoController()
     
     router.post("payload") { req -> Future<HTTPStatus> in
-        return try req.content.decode(PullRequest.self).map(to: HTTPStatus.self, { pullRequest in
-            print(pullRequest.review.pull_request_url)
+        print(req.http.headers["X-GitHub-Event"].first ?? "Unknown Event")
+        let event = handleEvent(req: req)
+        switch event {
+            
+        case .STATUS: break
+            
+        case .PULL_REQUEST:
+            return try req.content.decode(Payload.self).map(to: HTTPStatus.self, { payload in
+                print(payload.pull_request.number)
+                return .ok
+            })
+        case .UNKNOWN: break
+        }
+        return try req.content.decode(Payload.self).map(to: HTTPStatus.self, { payload in
+            print(payload.review.pull_request_url)
             return .ok
         })
     }
@@ -25,4 +38,8 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+func handleEvent(req: Request) -> EventType {
+    return EventType(rawValue: req.http.headers["X-GitHub-Event"].first ?? "unknown") ?? EventType.UNKNOWN
 }
